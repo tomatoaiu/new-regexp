db9ae9da6d9465638d9d8029ce2edb20a92bc109
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-12-17 23:41:45 +0900

    refactor(js-regexp): to export function

--- src/javascript/regexp.ts
+++ src/javascript/regexp.ts
@@ -1,30 +1,23 @@
-function rawtext (): string {
+export function rawtext (): string {
   const reg: RegExp = /foo/;
 
   return 'foobarbaz'.match(reg).pop();
 }
 
-function lowerCaseOneAlphabet (): string {
+export function lowerCaseOneAlphabet (): string {
   const reg: RegExp = /[a-z]/;
 
   return '123foo456'.match(reg).pop();
 }
 
-function upperCaseOneAlphabet (): string {
+export function upperCaseOneAlphabet (): string {
   const reg: RegExp = /[A-Z]/;
 
   return '123fOo456'.match(reg).pop();
 }
 
-function numberText (): string {
+export function numberText (): string {
   const reg: RegExp = /[0-9]/;
 
   return '123fOo456'.match(reg).pop();
-}
-
-export {
-  rawtext,
-  lowerCaseOneAlphabet,
-  numberText,
-  upperCaseOneAlphabet,
 }
